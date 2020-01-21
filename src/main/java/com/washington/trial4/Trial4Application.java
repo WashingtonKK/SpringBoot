b6c7e680c660003016ec90b23b3466fdab6f5967
Washington Kamadi Kigani <washingtonkigan@gmail.com>
2020-01-21 04:37:47 +0300

    Added additional files via upload

---
+++ src/main/java/com/washington/trial4/Trial4Application.java
@@ -0,0 +1,13 @@
+package com.washington.trial4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trial4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trial4Application.class, args);
+	}
+
+}
---
+++ src/main/java/dao/FakePersonDataAcessService.java
@@ -0,0 +1,15 @@
+package dao;
+
+import java.util.UUID;
+
+import model.Person;
+
+public class FakePersonDataAcessService implements PersonDao {
+
+	@Override
+	public int insertPerson(UUID id, Person person) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
